9f069b25b1ef13b868d2df62a6679be4bd4e72c0
Brendon Dodd <17181659+brendoncdodd@users.noreply.github.com>
2024-10-11 15:13:22 -0400

    Rewrote a bunch of functions for style and better error handling.

--- strequests.go
+++ strequests.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"io/fs"
@@ -49,88 +48,132 @@ func (self *Vector2) Distance(other Vector2) float64 {
 func init() {
 }
 
-func decodeToken(data []byte) ([]byte, error) {
+// Get a spacetraders.io agent token from some JSON.
+// Give this some JSON that follows the pattern:
+// { "data": { "token": [TOKEN] } }
+func decodeToken(JSONdata []byte) ([]byte, error) {
 	var jsonMap map[string]any
 	error_prefix := "While trying to decode JSON and get token."
 
-	err := json.Unmarshal(data, &jsonMap)
+	err := json.Unmarshal(JSONdata, &jsonMap)
 	if err != nil {
-		return []byte{}, fmt.Errorf(
-			"%s%w",
-			error_prefix,
-			err,
-		)
+		return []byte{}, fmt.Errorf("%s %w", error_prefix, err)
 	}
 
-	if data, ok := jsonMap["data"].(map[string]any); ok {
-		if token, ok := data["token"].(string); ok {
-			return []byte(token), err
-		} else {
-			return nil, fmt.Errorf(
-				"%s data[token] is not string.",
-				error_prefix,
-			)
-		}
-	} else {
+	if data, ok := jsonMap["data"].(map[string]any); !ok {
 		return nil, fmt.Errorf(
 			"%s data is not a map of strings.",
 			error_prefix,
 		)
+	} else if token, ok := data["token"].(string); !ok {
+		return nil, fmt.Errorf(
+			"%s data.token is not string.",
+			error_prefix,
+		)
+	} else {
+		return []byte(token), err
 	}
 }
 
+// Creates a spacetraders.io agent.
+// Writes the (JSON) response body to a file: savefiles/[agent].json
+// Returns the entire response body (including the token)
 func createAgent(agent string, faction string) (string, error) {
-	responseBuffer := make([]byte, 20000)
+	const BUFFER_SIZE = 20000
+	error_prefix := "Trying to create new agent."
+	responseBody := make([]byte, BUFFER_SIZE)
 	agentMap := make(map[string]string)
 	agentMap["symbol"] = agent
 	agentMap["faction"] = faction
 
 	agent_json_bytes, err := json.Marshal(agentMap)
 	if err != nil {
-		return "", errors.New("Trying to create JSON for new agent request.\t" + err.Error())
+		return "", fmt.Errorf("%s Marshalling JSON. %w",
+			error_prefix,
+			err,
+		)
 	}
 
-	resp, err := http.Post(url_base+"/v2/register", "application/json", strings.NewReader(string(agent_json_bytes)))
+	resp, err := http.Post(
+		url_base+"/v2/register",
+		"application/json",
+		strings.NewReader(string(agent_json_bytes)),
+	)
 	if err != nil {
-		return "", errors.New("Trying to POST new agent request: \n\t" + string(agent_json_bytes) + "\n\t" + err.Error())
+		return "", fmt.Errorf(
+			"%s Trying to POST new agent request.\n%s\n%w",
+			error_prefix,
+			string(agent_json_bytes),
+			err,
+		)
 	}
 	defer resp.Body.Close()
 
-	_, err = resp.Body.Read(responseBuffer)
+	bodySize, err := resp.Body.Read(responseBody)
 	if err != nil {
-		return string(responseBuffer), errors.New("Trying to read body of new agent request: " + err.Error())
+		return string(responseBody), fmt.Errorf(
+			"%s Trying to read body of new agent request. %w",
+			error_prefix,
+			err,
+		)
 	}
-
-	responseBody := bytes.TrimRight(responseBuffer, "\x00")
+	responseBody = bytes.TrimRight(responseBody, "\x00")
 	responseBody = append(responseBody, byte('\n'))
 
 	err = os.WriteFile("savefiles/"+agent+".json", responseBody, fs.ModePerm)
 	if err != nil {
-		return string(responseBody), errors.New("Trying to write new agent file: " + err.Error())
+		return string(responseBody), fmt.Errorf(
+			"%s Trying to write new agent file. %w",
+			error_prefix,
+			err,
+		)
+	}
+
+	if bodySize >= BUFFER_SIZE {
+		return string(responseBody), fmt.Errorf(
+			"%s Response body too big for buffer (%i bytes).",
+			error_prefix,
+			BUFFER_SIZE,
+		)
 	}
 
 	return string(responseBody), err
 }
 
-// Not fully implemented. Returns the raw JSON and the request status.
-// Attempts to preserve the body of the template but if it fails gives it an empty one. We don't guard this. Even if you give this an extremely long body it will copy the whole thing twice.
-// Give it a template with the "Authorization: Bearer [token] header already added.
-func getAgentDetails(requestTemplate *http.Request) (agent string, status string, err error) {
-	bodyBuf := make([]byte, 10000) //This is the buffer for the response body. It's probably a good idea not to let the responder give us as much data as they want.
-	var jsonBuf []byte
+// Not fully implemented. Returns the raw JSON and the request status, in that order.
+// Give it an http.Request with the GET method and "Authorization: Bearer [token] header already added.
+//
+//	or nil to use token_GET
+//
+// Attempts to preserve the body of the template but if it fails gives it an empty one.
+//
+//	We don't guard this. Even if you give this an extremely long body it will copy the whole thing twice.
+func getAgentDetails(requestTemplate *http.Request) (string, string, error) {
+	const BUFFER_SIZE = 10000
+	error_prefix := "Trying to get agent details."
+	responseBody := make([]byte, BUFFER_SIZE)
 
-	oldBody, err := io.ReadAll(requestTemplate.Body)
-	if err != nil {
-		oldBody = []byte("")
+	if requestTemplate == nil {
+		requestTemplate = token_GET
 	}
 
-	err = token_GET.Body.Close()
+	oldRequestBody, err := io.ReadAll(requestTemplate.Body)
 	if err != nil {
-		return "", "", errors.New("While trying getAgentDetails. While trying to close token_GET.Body after cloning.\n" + err.Error())
+		oldRequestBody = []byte("")
 	}
 
-	req := token_GET.Clone(token_GET.Context())
-	token_GET.Body = io.NopCloser(bytes.NewReader(oldBody))
+	req := requestTemplate.Clone(requestTemplate.Context())
+
+	err = requestTemplate.Body.Close()
+	if err != nil {
+		return "", "", fmt.Errorf(
+			"%s Closing request template body after cloning. %w",
+			error_prefix,
+			err,
+		)
+	}
+	requestTemplate.Body = io.NopCloser(bytes.NewReader(oldRequestBody))
+
 	req.Body = io.NopCloser(strings.NewReader(""))
 	defer req.Body.Close()
 
@@ -138,64 +181,118 @@ func getAgentDetails(requestTemplate *http.Request) (agent string, status string
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", resp.Status, errors.New("While trying to send GET request for agent:\t" + err.Error())
+		return "", resp.Status, fmt.Errorf(
+			"%s Trying to send GET request. %w",
+			error_prefix,
+			err,
+		)
 	}
 	defer resp.Body.Close()
 
-	status = resp.Status
-
-	bodySize, err := resp.Body.Read(bodyBuf)
-	if len(bodyBuf) >= bodySize {
-		jsonBuf = make([]byte, bodySize)
-		copy(jsonBuf, bodyBuf)
-	} else {
-		jsonBuf = bodyBuf
+	bodySize, err := resp.Body.Read(responseBody)
+	responseBody = bytes.TrimRight(responseBody, "\x00")
+	if err != nil {
+		return string(responseBody), resp.Status, fmt.Errorf(
+			"%s Trying to read response body. %w",
+			error_prefix,
+			err,
+		)
 	}
-	agent = string(jsonBuf)
+	if bodySize >= BUFFER_SIZE {
+		return string(responseBody), resp.Status, fmt.Errorf(
+			"%s Response body too big for buffer (%i bytes).",
+			error_prefix,
+			BUFFER_SIZE,
+		)
+	}
 
-	return
+	return string(responseBody), resp.Status, err
 }
 
 // Not implemented yet
 // TODO: Get ship location.
 // TODO: Get symbols and locations of waypoint with traits.
 // TODO: Remove lines that assign test values.
-func findNearestWaypointWithTraits(shipSymbol string, traits []string) (waypointSymbol string, err error) {
+func findNearestWaypointWithTraits(requestTemplate *http.Request, shipSymbol string, traits []string) (waypointSymbol string, err error) {
+	const BUFFER_SIZE = 20000
+	var shipLocation Vector2
+	var rawShipObject map[string]any
+	error_prefix := "Trying to find nearest waypoint with traits."
 	minDistance := math.MaxFloat64
 	waypoints := make(map[string]Vector2)
-	responseBuffer := make([]byte, 20000)
+	responseBody := make([]byte, BUFFER_SIZE)
+
+	if requestTemplate == nil {
+		requestTemplate = token_GET
+	}
 
 	//Convert traits to a format we can use in the request.
 
 	//Get the symbols and locations of all waypoints in the system
 
 	//Get the ship location (where you want to find nearest to) https://api.spacetraders.io/v2/my/ships/{shipSymbol}/nav
-	req := token_GET.Clone(token_GET.Context())
-	token_GET.Body.Close()
+	req := requestTemplate.Clone(requestTemplate.Context())
+	requestTemplate.Body.Close()
 
-	token_GET.Body = io.NopCloser(strings.NewReader(""))
+	requestTemplate.Body = io.NopCloser(strings.NewReader(""))
 	req.Body = io.NopCloser(strings.NewReader(""))
 	defer req.Body.Close()
+
 	req.URL.Path = fmt.Sprintf("/v2/my/ships/%s/nav", shipSymbol)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		//TODO: Handle failed request
+		err = fmt.Errorf(
+			"%s Request failed. %w",
+			error_prefix,
+			err,
+		)
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+		return
 	}
 	defer resp.Body.Close()
 
-	shipRequestlen, _ := resp.Body.Read(responseBuffer)
-	shipRequestBody := make([]byte, shipRequestlen)
-
-	copy(shipRequestBody, responseBuffer)
-
-	var rawShipObject map[string]any
-	err = json.Unmarshal(shipRequestBody, &rawShipObject)
+	responseBodySize, err := resp.Body.Read(responseBody)
 	if err != nil {
-		//TODO: Handle json unmarshal error
+		err = fmt.Errorf(
+			"%s Trying to read response body. %w",
+			error_prefix,
+			err,
+		)
+		return
+	}
+	if responseBodySize >= BUFFER_SIZE {
+		err = fmt.Errorf(
+			"%s Response too big for buffer (%i bytes)\n%s",
+			error_prefix,
+			BUFFER_SIZE,
+			responseBody,
+		)
+		return
+	}
+	responseBody = bytes.TrimRight(responseBody, "\x00")
+
+	err = json.Unmarshal(responseBody, &rawShipObject)
+	if err != nil {
+		err = fmt.Errorf(
+			"%s Failed to unmarshal JSON:\n %s\n%w",
+			error_prefix,
+			string(responseBody),
+			err,
+		)
 	}
 
-	shipLocation := Vector2{rawShipObject["data"].(map[string]any)["route"].(map[string]any)["destination"].(map[string]int)["x"], rawShipObject["data"].(map[string]any)["route"].(map[string]any)["destination"].(map[string]int)["y"]}
+	if data, ok := rawShipObject["data"].(map[string]any); !ok {
+		// rawShipObject["data"] not map[string]any
+	} else if route, ok := data["route"].(map[string]any); !ok {
+		// rawShipObject["data"]["route"] not map[string]any
+	} else if destination, ok := route["destination"].(map[string]int); !ok {
+		// rawShipObject["data"]["route"]["destination"] not map[string]int
+	} else {
+		shipLocation = Vector2{destination["x"], destination["y"]}
+	}
 
 	waypoints["DERP"] = Vector2{3, 4}     //Delete when we actually have waypoints
 	waypoints["DERP2"] = Vector2{7, 1024} //Delete when we actually have waypoints
